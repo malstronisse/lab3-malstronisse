4f6a01a02f63de5741efb91db59da12ed7a8bb01
Maelstrognome <Maelstrognome@10.31.48.79>
2017-11-15 12:37:56 -0400

    1. Switched the string in: JFrame myFrame = new JFrame("Hello, world!");
    with another string, of my name.
    
    2.
    Switched the coordinates in:
    myFrame.setLocation(200, 200);
    
    to an approximation of the center of my screen.
    
    3.
    switched the x,y for:
    myFrame.setSize(400, 100);
    
    to the designated (200,200)

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,22 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
+		JFrame myFrame = new JFrame("John Paul Pacheco-Soto");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(850, 400);
+		myFrame.setSize(200, 200);
+		myFrame.setVisible(true);
+	}
+}
+
+/*
+public class Lab3Exercises {
+	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(200, 200);
 		myFrame.setSize(400, 100);
 		myFrame.setVisible(true);
 	}
 }
+*/
