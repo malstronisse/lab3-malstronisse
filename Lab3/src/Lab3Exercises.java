fe11a37de9fa6b3bdc56ed22e0f665358964446a
Maelstrognome <Maelstrognome@10.31.48.79>
2017-11-15 12:42:41 -0400

    Final version

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -9,15 +9,3 @@ public class Lab3Exercises {
 		myFrame.setVisible(true);
 	}
 }
-
-/*
-public class Lab3Exercises {
-	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
-		myFrame.setVisible(true);
-	}
-}
-*/
